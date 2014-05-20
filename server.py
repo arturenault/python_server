e051e5bdd1a5357eb35b5ed074952992ad9ed8cb
Artur Renault <artur.renault@gmail.com>
2014-05-20 01:28:52 -0300

    Socket options and listening

--- server.py
+++ server.py
@@ -5,7 +5,16 @@ from sys import argv
 import socket
 
 if len(argv) != 3:
-    print "Usage: ./server.py [host] [port]"
+    print "Usage: ./server.py [port] [root]"
     exit(1)
 
-script, host, port = argv
+host = ""
+script, port, root = argv
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sock.bind(host, port)
+
+sock.listen(1)
