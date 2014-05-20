5572727315358441c9436470a9e9676ae9cb47f4
Artur Renault <artur.renault@gmail.com>
2014-05-20 01:39:25 -0300

    Serve default message

--- server.py
+++ server.py
@@ -4,17 +4,36 @@ import sys
 from sys import argv
 import socket
 
-if len(argv) != 3:
-    print "Usage: ./server.py [port] [root]"
+if len(argv) != 2:
+    print "Usage: ./server.py [port]"
     exit(1)
 
-host = ""
-script, port, root = argv
+host = ''
+port = int(argv[1])
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+servSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sock.bind(host, port)
+servSock.bind((host, port))
 
-sock.listen(1)
+servSock.listen(1)
+
+while True:
+    clntSock, clntAddr = servSock.accept()
+    clntFile = clntSock.makefile("rw", 0)
+
+    line = clntFile.readline().strip()
+
+    clntFile.write('HTTP/1.0 200 OK\n\n')
+    clntFile.write('<html><head><title>Welcome %s!</title></head>' %(str(clntAddr)))
+    clntFile.write('<body><h1>Follow the link...</h1>')
+    clntFile.write('All the server needs to do is ')
+    clntFile.write('to deliver the text to the socket. ')
+    clntFile.write('It delivers the HTML code for a link, ')
+    clntFile.write('and the web browser converts it. <br><br><br><br>')
+    clntFile.write('<font size="7"><center> <a href="http://python.about.com/index.html">Click me!</a> </center></font>')
+    clntFile.write('<br><br>The wording of your request was: "%s"' %(line))
+    clntFile.write('</body></html>')
+
+    clntFile.close()
